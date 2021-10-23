c1add783737d0eab5c071c721cf4d3afc76147b6
Unknown246 <92207162+Unknown246@users.noreply.github.com>
2021-10-23 13:54:13 +0500

    123

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+var gravity = Vector2(0,980)
+var vel = Vector2()
+var OAT = 0
+var on_floor = false
+var speed = 200
+var jump = 400
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	OAT += delta
+	vel += gravity * delta 
+	if is_on_floor():
+		OAT = 0
+	on_floor = OAT < 0.1 
+	vel = move_and_slide(vel,Vector2(0, -1),25.0)
+	var target_vel = 0 
+	if Input.is_action_pressed("ui_left"):
+		target_vel -= 1
+	if Input.is_action_pressed("ui_right"):
+		target_vel += 1
+	target_vel *= speed
+	vel.x = lerp(vel.x,target_vel,0.3)
+	
+	if Input.is_action_pressed("ui_up") and on_floor:
+		vel.y = -jump
+
